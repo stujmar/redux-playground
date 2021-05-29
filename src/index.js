aeb822a84a34445c18ed6176e6ce3e9c5de2c3fb
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-28 22:04:31 -0500

    [sm] implemented browser router in the index.js and got page working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import HomePage from "./components/home/HomePage";
+import { BrowserRouter as Router } from "react-router-dom";
 
-render(<HomePage />, document.getElementById("app"));
+render(
+  <Router>
+    <HomePage />
+  </Router>,
+  document.getElementById("app")
+);
