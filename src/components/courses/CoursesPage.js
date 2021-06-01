87f11be5c275128640112d7e252cd63ff952b08a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-31 19:51:52 -0500

    [sm] created a crude course form in the coursepage component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -11,7 +11,14 @@ class CoursesPage extends React.Component {
         };
     }
     render() {
-        return <h2>Courses Page</h2>
+        return (
+            <form>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title}/>
+                <input type="submit" value="Save" />
+            </form>
+        )
     }
 }
 
