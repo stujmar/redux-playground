856c1eeb0a50a7cc04bad6f650300d31269e6aad
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-31 19:22:55 -0500

    [sm] installed react-redux and got correct Provider component wrapping the App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
-import { Provider } from "redux";
+import { Provider } from "react-redux";
 
 render(
   <Provider store={this.props.store}>
