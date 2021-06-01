62d12734099ff32c46e81d569aaa32379435ed78
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-31 19:48:35 -0500

    [sm] added state to the class based CoursesPage component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            course: {
+                title: ""
+            }
+        };
+    }
     render() {
         return <h2>Courses Page</h2>
     }
