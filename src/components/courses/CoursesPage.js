89a8e64b46853bd9cf037511c14a0053a2e1868d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-31 20:09:20 -0500

    [sm] got pretty vanilla form up and running in class component now for redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -11,11 +11,16 @@ class CoursesPage extends React.Component {
         const course = {...this.state.course, title: event.target.value};
         this.setState({ course }); // Object shorthand syntax
         // this.setState({ course: course });
-    }
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(this.state.course.title);
+    };
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Courses</h2>
                 <h3>Add Course</h3>
                 <input type="text" onChange={this.handleChange} value={this.state.course.title}/>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
 render(
   // <Provider store={this.props.store}>
     <Router>
       <App />
     </Router>,
-  {/* </Provider>, */}
+  /* </Provider>, */
   document.getElementById("app")
 );
