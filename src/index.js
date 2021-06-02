d6d7e363985a154c656cc23c882ab7c2dbe36cd4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-01 21:54:26 -0500

    [sm] wrapped add in ReduxProvider and brought in store from configureStore

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
-// import { Provider } from "react-redux";
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  // <Provider store={this.props.store}>
+  <ReduxProvider store={store}>
     <Router>
       <App />
-    </Router>,
-  /* </Provider>, */
+    </Router>
+  </ReduxProvider>, 
   document.getElementById("app")
 );
