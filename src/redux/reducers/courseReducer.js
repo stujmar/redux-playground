c93a66fc8c39e345bffa431f2ffc0a063fb0804e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-01 21:40:21 -0500

    [sm] created an index.js in the reducers folder to build a root reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,7 @@
 export default function courseReducer(state = [], action) {
     switch(action.type) {
         case "CREATE_COURSE":
-            return [...state, { ...action.course}];
+            return [...state, { ...action.course }];
         default:
             return state;
     }
