33135d0af5b441dca837511442138cb797bbb77d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-02 21:10:34 -0500

    [sm] got it working again with actions object being passed as props

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux'
 
 class CoursesPage extends React.Component {
     state = {
@@ -19,7 +20,7 @@ class CoursesPage extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
         // alert(this.state.course.title);
-        this.props.createCourse(this.state.course);
+        this.props.actions.createCourse(this.state.course);
     };
 
     render() {
@@ -45,12 +46,13 @@ function mapStateToProps(state){
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        createCourse: course => dispatch(courseActions.createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch) // use bind action creator!?
+        // createCourse: course => dispatch(courseActions.createCourse(course))
     }
 }
 
